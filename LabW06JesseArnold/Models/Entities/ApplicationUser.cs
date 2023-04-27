b16be8df2a30052f8323dd89af71a579cef82b62
JessAye <jess@lumos.net>
2023-04-26 22:22:39 -0400

    First

---
+++ LabW06JesseArnold/Models/Entities/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Runtime.CompilerServices;
+using System.ComponentModel.DataAnnotations;
+namespace LabW06JesseArnold.Models.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public UserManager<ApplicationUser> UserManager { get; set; }   
+    }
+}
--- LabW06JesseArnold/Services/ApplicationDbContext.cs
+++ LabW06JesseArnold/Services/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LabW06JesseArnold.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LabW06JesseArnold.Services
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
