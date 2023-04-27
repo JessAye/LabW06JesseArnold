159aafd2e1a73f112e48a9d722565520a589aea3
JessAye <jess@lumos.net>
2023-04-26 22:01:23 -0400

    Add project files.

---
+++ LabW06JesseArnold/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using LabW06JesseArnold.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LabW06JesseArnold.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
