472a0c3b4fb18cd8ee754f18d723cceba69093d5
Jesse Arnold <ARNOLDJT@ETSU.EDU>
2023-04-27 16:23:33 -0400

    Push before dead laptop

--- LabW06JesseArnold/Controllers/HomeController.cs
+++ LabW06JesseArnold/Controllers/HomeController.cs
@@ -30,5 +30,6 @@ namespace LabW06JesseArnold.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
---
+++ LabW06JesseArnold/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using LabW06JesseArnold.Models.ViewModels;
+using LabW06JesseArnold.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabW06JesseArnold.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepo;
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepo.ReadAllAsync();
+            var userList = users
+               .Select(u => new UserListVM
+               {
+                   Email = u.Email,
+                   UserName = u.UserName,
+                   NumberOfRoles = u.Roles.Count,
+                   RoleNames = string.Join(",", u.Roles.ToArray())
+               });
+            return View(userList);
+        }
+
+    }
+}
