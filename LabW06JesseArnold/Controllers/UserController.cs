520fba7b16df460fbe49cff7c1ca7958fc5d6689
Jesse Arnold <ARNOLDJT@ETSU.EDU>
2023-04-27 16:38:09 -0400

    fuck yeah

--- LabW06JesseArnold/Controllers/UserController.cs
+++ LabW06JesseArnold/Controllers/UserController.cs
@@ -6,10 +6,16 @@ namespace LabW06JesseArnold.Controllers
 {
     public class UserController : Controller
     {
-        private readonly IUserRepository _userRepo;
+
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        //Creat
         public async Task<IActionResult> Index()
         {
-            var users = await _userRepo.ReadAllAsync();
+            var users = await _userRepository.ReadAllAsync();
             var userList = users
                .Select(u => new UserListVM
                {
